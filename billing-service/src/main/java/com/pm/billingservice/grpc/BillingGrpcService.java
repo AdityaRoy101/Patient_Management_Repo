54c97c3e5bfa011c17e02de7794377973c047a3b
Aditya Roy <mr.adityaroy101@gmail.com>
2025-05-01 22:45:01 +0530

    Added GRPC Client and Server config for both Billing and Patient with Docker Config.

--- billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -1,17 +1,20 @@
 package com.pm.billingservice.grpc;
 
+import billing.BillingRequest;
 import billing.BillingResponse;
-import billing.BillingServiceGrpc.BillingServiceImplBase;
+import billing.BillingServiceGrpc;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @GrpcService
-public class BillingGrpcService extends BillingServiceImplBase {
+public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
+
     private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
 
-    public void createBillingAccount(billing.BillingRequest billingRequest, StreamObserver<billing.BillingResponse> responseObserver) {
+    @Override
+    public void createBillingAccount(BillingRequest billingRequest, StreamObserver<BillingResponse> responseObserver) {
 
         log.info("createBillingAccount request received {}", billingRequest.toString());
 
